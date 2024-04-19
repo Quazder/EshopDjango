974b9d2d48eb5301dfc7f72e79b621410d253dd9
Your Name <Quazdr@seznam.cz>
2024-04-19 05:49:23 +0200

    fotky fungujou i closeup

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,21 @@
 document.getElementById('navbarDropdown').addEventListener('click', function (event) {
     event.preventDefault();
 });
+
+window.onload = function() {
+    var cards = document.querySelectorAll('.card.h-100');
+    cards.forEach(function(card) {
+        card.addEventListener('mouseenter', function() {
+            var imgOriginal = card.querySelector('.product-image.original');
+            var imgCloseup = card.querySelector('.product-image.closeup');
+            imgOriginal.style.opacity = 0;
+            imgCloseup.style.opacity = 1;
+        });
+        card.addEventListener('mouseleave', function() {
+            var imgOriginal = card.querySelector('.product-image.original');
+            var imgCloseup = card.querySelector('.product-image.closeup');
+            imgOriginal.style.opacity = 1;
+            imgCloseup.style.opacity = 0;
+        });
+    });
+};
