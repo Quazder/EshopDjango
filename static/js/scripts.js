c1b6d1e80bab6372a21646eb7e653de9785f95ce
Your Name <Quazdr@seznam.cz>
2024-04-19 05:58:53 +0200

    navbar z půlky fixlý tak jak bych chtěl

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,15 @@
-document.getElementById('navbarDropdown').addEventListener('click', function (event) {
-    event.preventDefault();
-});
+/*Zrušení možností kliknout na dropdown menu - nefunguje tak jak by měl*/
 
+/*Funkce pro změnění fotky při hover efektu*/
 window.onload = function() {
+    var dropdown = document.getElementById('navbarDropdown');
+    if (dropdown) {
+        dropdown.addEventListener('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        }, true); // Add true to make the event handler execute in the capturing phase
+    }
+
     var cards = document.querySelectorAll('.card.h-100');
     cards.forEach(function(card) {
         card.addEventListener('mouseenter', function() {
